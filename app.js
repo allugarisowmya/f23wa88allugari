dc6768bc70477b1cc7a3c4274a2edef08eed71c3
Sowmya Allugari <allugarisowmya2001@gmail.com>
2023-10-25 18:27:47 -0500

    modified app.js in the server

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
-var computationRouter = require('./route/computation');
+var computationRouter = require('./routes/computation');
+
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
